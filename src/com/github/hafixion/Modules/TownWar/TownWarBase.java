28b0e711db8dab0b6ef579ca1a9292f60642e0ed
Shehab <shehabhafez3@gmail.com>
2020-10-30 00:29:18 +0200

    TownWar listener and base additions

--- src/com/github/hafixion/Modules/TownWar/TownWarBase.java
+++ src/com/github/hafixion/Modules/TownWar/TownWarBase.java
@@ -6,12 +6,14 @@ import com.palmergames.bukkit.towny.object.Town;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 public class TownWarBase {
     public static Path database = Paths.get("plugins/Feudalism/data/townwar");
@@ -50,6 +52,122 @@ public class TownWarBase {
         return result;
     }
 
-    
+    public static void AddTowntoWarList(UUID town) {
+        // prepare config
+        YamlConfiguration wardata = new YamlConfiguration();
+        try {
+            wardata.load(FileUtils.warlist);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        List<String> towns = new ArrayList<>(Arrays.asList(wardata.getString("towns").split("-")));
+        if (!towns.contains(town.toString())) {
+            towns.add(town.toString());
+            wardata.set("towns", String.join("-", towns));
+        }
+    }
+
+    public static void RemoveTownfromWarList(UUID town) {
+        // prepare config
+        YamlConfiguration wardata = new YamlConfiguration();
+        try {
+            wardata.load(FileUtils.warlist);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        List<String> towns = new ArrayList<>(Arrays.asList(wardata.getString("towns").split("-")));
+        if (towns.contains(town.toString())) {
+            towns.remove(town.toString());
+            wardata.set("towns", String.join("-", towns));
+        }
+    }
+
+    public static void RemoveNationfromWarList(UUID nation) {
+        // prepare config
+        YamlConfiguration wardata = new YamlConfiguration();
+        try {
+            wardata.load(FileUtils.warlist);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        List<String> nations = new ArrayList<>(Arrays.asList(wardata.getString("nations").split("-")));
+        if (nations.contains(nation.toString())) {
+            nations.remove(nation.toString());
+            wardata.set("towns", String.join("-", nations));
+        }
+    }
+
+    public static void AddNationtoWarList(UUID nation) {
+        // prepare config
+        YamlConfiguration wardata = new YamlConfiguration();
+        try {
+            wardata.load(FileUtils.warlist);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        List<String> nations = new ArrayList<>(Arrays.asList(wardata.getString("nations").split("-")));
+        if (!nations.contains(nation.toString())) {
+            nations.remove(nation.toString());
+            wardata.set("towns", String.join("-", nations));
+        }
+    }
+
+    public static List<File> getWarFiles(UUID uuid) {
+        List<File> files = null;
+        YamlConfiguration config = new YamlConfiguration();
+
+        for (File data : database.toFile().listFiles()) {
+            try {
+                config.load(data);
+                if (config.getString("attacker") == uuid.toString() || config.getString("defender") == uuid.toString()) {
+                    files.add(data);
+                }
+            } catch (IOException | InvalidConfigurationException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return files;
+    }
+
+    public static List<UUID> getDefenders(UUID uuid) {
+        List<UUID> towns = null;
+        YamlConfiguration config = new YamlConfiguration();
+
+        for (File data : database.toFile().listFiles()) {
+            try {
+                config.load(data);
+                if (config.getString("attacker") == uuid.toString()) {
+                    towns.add(UUID.fromString(config.getString("defender")));
+                }
+            } catch (IOException | InvalidConfigurationException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return towns;
+    }
+
+    public static List<UUID> getAttackers(UUID uuid) {
+        List<UUID> nations = null;
+        YamlConfiguration config = new YamlConfiguration();
+
+        for (File data : database.toFile().listFiles()) {
+            try {
+                config.load(data);
+                if (config.getString("defender") == uuid.toString()) {
+                    nations.add(UUID.fromString(config.getString("attacker")));
+                }
+            } catch (IOException | InvalidConfigurationException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return nations;
+    }
 
 }
--- src/com/github/hafixion/Modules/TownWar/TownWarListener.java
+++ src/com/github/hafixion/Modules/TownWar/TownWarListener.java
@@ -1,13 +1,29 @@
 package com.github.hafixion.Modules.TownWar;
 
+import com.github.hafixion.Modules.Ruin.Events.RuinEvent;
 import com.github.hafixion.Utils.ChatInfo;
+import com.github.hafixion.Utils.FileUtils;
+import com.palmergames.bukkit.towny.TownyMessaging;
 import com.palmergames.bukkit.towny.TownyUniverse;
-import com.palmergames.bukkit.towny.event.TownSpawnEvent;
+import com.palmergames.bukkit.towny.event.*;
 import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
+import com.palmergames.bukkit.towny.object.Nation;
+import com.palmergames.bukkit.towny.object.Town;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
 public class TownWarListener implements Listener {
+    public Nation mergingnation;
+    public Town mergingcapital;
 
     @EventHandler
     public void onTownTeleport(TownSpawnEvent event) throws NotRegisteredException {
@@ -23,4 +39,102 @@ public class TownWarListener implements Listener {
         }
 
     }
+
+    @EventHandler
+    // when a dumbass invites a town at war
+    public void onNationInvite(NationInviteTownEvent event) {
+        if (TownWarBase.isTownAtWar(event.getInvite().getReceiver())) {
+            event.getInvite().decline(true);
+            event.getInvite().getDirectSender().sendMessage(ChatInfo.color("&c" + event.getInvite().getReceiver().getName() + " is at war, they cannot join a town at this time."));
+        }
+    }
+
+    @EventHandler
+    // when a dumbass joins a nation at war
+    public void onNationJoin(NationAddTownEvent event) {
+        if (TownWarBase.isNationAtWar(event.getNation())) {
+            TownWarBase.AddTowntoWarList(event.getTown().getUuid());
+        }
+    }
+
+    @EventHandler
+    // when a town leaves a nation at war (somehow)
+    public void onNationLeave(NationRemoveTownEvent event) {
+        // this is just to make sure it wasn't a merge
+        if (!event.getTown().hasNation()) {
+            if (TownWarBase.isNationAtWar(event.getNation())) {
+                TownWarBase.RemoveTownfromWarList(event.getTown().getUuid());
+            }
+        }
+    }
+
+    // Merging stuff and for when a nation is deleted, fuck towny not adding a merge event
+
+    @EventHandler
+    public void onNationPreDelete(PreDeleteNationEvent event) throws NotRegisteredException {
+        mergingnation = event.getNation();
+        mergingcapital = event.getNation().getCapital();
+
+        if (TownWarBase.isNationAtWar(event.getNation())) {
+            TownWarBase.RemoveNationfromWarList(event.getNation().getUuid());
+
+            // removing towns from being marked as at war
+            for (Town town : event.getNation().getTowns()) {
+                TownWarBase.RemoveTownfromWarList(town.getUuid());
+            }
+
+            // deleting all war files related to it
+            for (File file : TownWarBase.getWarFiles(event.getNation().getUuid())) {
+                file.delete();
+            }
+
+            // broadcast a message announcing peace
+            for (UUID uuid : TownWarBase.getDefenders(event.getNation().getUuid())) {
+                Town town = TownyUniverse.getInstance().getDataSource().getTown(uuid);
+                TownyMessaging.sendGlobalMessage(ChatInfo.color("&b" + event.getNationName() + " has collapsed, " + town.getName() + " is now at peace with them."));
+            }
+        }
+    }
+
+    @EventHandler
+    public void onNationDelete(DeleteNationEvent event) throws NotRegisteredException {
+        // prepare config
+        YamlConfiguration wardata = new YamlConfiguration();
+        try {
+            wardata.load(FileUtils.warlist);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        List<String> wars = new ArrayList<>(Arrays.asList(wardata.getString("nations").split("-")));
+        if (wars.contains(event.getNationUUID().toString())) {
+            if (mergingcapital.hasNation()) {
+                TownWarBase.RemoveNationfromWarList(event.getNationUUID());
+                TownWarBase.AddNationtoWarList(mergingcapital.getNation().getUuid());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onTownRuin(RuinEvent event) throws NotRegisteredException, IOException, InvalidConfigurationException {
+        int warscore = 0;
+        Town town = event.getTown();
+        YamlConfiguration config = new YamlConfiguration();
+        Nation victor = null;
+
+        if (TownWarBase.isTownAtWar(town)) {
+            for (UUID uuid : TownWarBase.getAttackers(town.getUuid())) {
+                Nation nation = TownyUniverse.getInstance().getDataSource().getNation(uuid);
+
+                for (File file : TownWarBase.getWarFiles(town.getUuid())) {
+                    config.load(file);
+                    if (warscore < config.getInt("warscore")) {
+                        warscore = config.getInt("warscore");
+                        victor = nation;
+                    }
+                }
+            }
+        }
+    }
+
 }
